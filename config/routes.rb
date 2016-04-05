820d6ca81f0a10da9f774dc6e48dc1f2075871fd
Milo Vrach <morelo87@gmail.com>
2016-04-05 17:38:50 +0200

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   
-  root to: 'static_pages/home', via: :get
+  
+
+  root to: 'static_pages#home', via: :get
  
+  get  'signup' => 'users#new'
+  get 'help' => 'static_pages#help'
 
-  get 'static_pages/help'
+  get 'about' =>  'static_pages#about'
 
-  get 'static_pages/about'
+  get 'contact' =>  'static_pages#contact'
 end
