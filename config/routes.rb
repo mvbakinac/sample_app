059298f36d27c7a3113c2575c624507b208cb2a4
Milo Vrach <morelo87@gmail.com>
2016-04-06 16:29:24 +0200

    Finish log in/log out

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,12 @@
 Rails.application.routes.draw do
-  
-  
-
   root to: 'static_pages#home', via: :get
  
-  get  'signup' => 'users#new'
-  get 'help' => 'static_pages#help'
-
-  get 'about' =>  'static_pages#about'
-
-  get 'contact' =>  'static_pages#contact'
-
+  get    'signup'  =>   'users#new'
+  get    'help'    =>   'static_pages#help'
+  get    'about'   =>   'static_pages#about'
+  get    'contact' =>   'static_pages#contact'
+  get    "login"   =>   'sessions#new'
+  post   "login"  =>    'sessions#create'
+  delete "logout"  =>   'sessions#destroy'
   resources :users
 end
